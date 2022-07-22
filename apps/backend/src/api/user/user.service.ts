30125c54651e9f25fa53364ee944693a58268f3c
Daniel <daniilterawars@gmail.com>
2022-07-22 15:27:43 +0300

    feat: auth

--- apps/backend/src/api/user/user.service.ts
+++ apps/backend/src/api/user/user.service.ts
@@ -7,18 +7,20 @@ export type User = any;
 export class UserService {
     private readonly users = [
         {
-            userId: 1,
-            username: 'john',
+            id: 1,
+            name: 'john',
+            email: 'john@mail.ru',
             password: 'changeme',
         },
         {
-            userId: 2,
-            username: 'maria',
+            id: 2,
+            name: 'maria',
+            email: 'maria@mail.ru',
             password: 'guess',
         },
     ];
 
     async findOne(username: string): Promise<User | undefined> {
-        return this.users.find(user => user.username === username);
+        return this.users.find(user => user.name === username) || this.users.find(user => user.email === username);
     }
 }
