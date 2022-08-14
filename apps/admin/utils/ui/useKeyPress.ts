8fe10e7fc79cd74966a663a882e831c3dd1e5e97
Daniel <daniilterawars@gmail.com>
2022-08-15 00:24:37 +0300

    feat(ui): useKeyPress

---
+++ apps/admin/utils/ui/useKeyPress.ts
@@ -0,0 +1,14 @@
+/**
+ * @Example
+ useKeypress(['ArrowLeft', 'ArrowRight'], (event) => {
+    if (event.key === 'ArrowLeft') {
+      moveLeft();
+    } else {
+      moveRight();
+    }
+  });
+ */
+
+export const useKeyPress = (keys, handler) => {
+  //
+}
