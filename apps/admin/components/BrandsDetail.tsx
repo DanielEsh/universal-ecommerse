b52bf5eb9439bb60b83957d27ecdb0065338afdc
Daniel <daniilterawars@gmail.com>
2022-08-21 12:17:18 +0300

    feat(brands): detail

--- apps/admin/components/BrandsDetail.tsx
+++ apps/admin/components/BrandsDetail.tsx
@@ -1,58 +1,59 @@
-import {useEffect, useState} from 'react'
-import { useRouter } from 'next/router'
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
-import { getBrandById } from '../service/brands.service'
-import { dateToString } from '../utils/dateToString'
+import { getBrandById } from "../service/brands.service";
+import { dateToString } from "../utils/dateToString";
 
-import { BaseInput } from './ui/inputs/BaseInput'
+import { BaseInput } from "./ui/inputs/BaseInput";
 
 export const BrandsDetail = () => {
-    const router = useRouter()
-    const [data, setData] = useState(null)
+  const router = useRouter();
+  const [data, setData] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await getBrandById(router.query.brandId as string)
-            setData(res.data)
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getBrandById(router.query.brandId as string);
+      setData(res.data);
+    };
 
-        fetchData().catch(console.error);
+    fetchData().catch(console.error);
 
-        console.log('DATA', data);
-    }, [])
+    console.log("DATA", data);
+  }, []);
 
-    return (
-        <div>
-            DETAIL
-            {
-                data && (
-                    <>
-                        <form>
-                            <div className="w-[380px]">
-                                <BaseInput 
-                                    label="Name"
-                                    defaultValue={data.name}
-                                />
-                            </div>
+  return (
+    <div className="flex h-full">
+      <div className="flex-1">
+        {data && (
+          <>
+            <form>
+              <div className="w-[380px]">
+                <BaseInput label="Name" defaultValue={data.name} />
+              </div>
 
-                            <div className="w-[380px]">
-                                <BaseInput 
-                                    label="Description"
-                                    defaultValue={data.description}
-                                />
-                            </div>
-                        </form>
-
-
-                        <div>
-                            {dateToString(new Date(data.created_at))}
-                        </div>
-                        <div>
-                            {dateToString(new Date(data.updated_at))}
-                        </div>
-                    </>
-                )
-            }
+              <div className="w-[380px]">
+                <BaseInput
+                  label="Description"
+                  defaultValue={data.description}
+                />
+              </div>
+            </form>
+          </>
+        )}
+      </div>
+      
+      <aside className="w-[280px]">
+        <div className="flex flex-col gap-4">
+          <div className="flex items-center justify-between bg-slate-200 p-4 rounded-md">
+            <span>Дата создания</span>{" "}
+            <span>{dateToString(new Date(data?.created_at))}</span>
+          </div>
+          <div className="flex items-center justify-between bg-slate-200 p-4 rounded-md">
+            <span>Дата обновления</span>{" "}
+            <span>{dateToString(new Date(data?.updated_at))}</span>
+          </div>
         </div>
-    )
-}
+      </aside>
+    </div>
+  );
+};
