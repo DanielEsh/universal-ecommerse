cfe61e9db5916c7bb6a8ec646be3d9fa55e1f91f
Daniel <daniilterawars@gmail.com>
2022-08-28 19:34:35 +0300

    feat(brands): CRUD

--- apps/backend/src/api/brands/controller/brands.controller.ts
+++ apps/backend/src/api/brands/controller/brands.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { BrandsService } from '@/api/brands/service/brands.service';
-import { Brand } from '@/api/brands/entities/brand.entity';
 import { CreateBrandDto } from '@/api/brands/dto/create-brand.dto';
 import { UpdateBrandDto } from '@/api/brands/dto/update-brand.dto';
 
 
 @Controller('brands')
 export class BrandsController {
-  constructor(public service: BrandsService) {}
+  constructor(private readonly brandsService: BrandsService) {}
+
+  @Post()
+  createBrand(@Body() body: CreateBrandDto) {
+    return this.brandsService.create(body);
+  }
 
   @Get()
   findAll() {
-    return BrandsService.findAll();
+    return this.brandsService.findAll();
+  }
+
+  @Get(':id')
+  findOneBrand(@Param('id') id: string) {
+    return this.brandsService.findOne(+id);
+  }
+
+  @Put(':id')
+  updateBrand(
+      @Param('id') id: string,
+      @Body() body: UpdateBrandDto,
+  ) {
+    return this.brandsService.update(+id, body);
+  }
+
+  @Delete(':id')
+  deleteBrand(@Param('id') id: string) {
+    return this.brandsService.delete(+id);
   }
 }
--- apps/backend/src/api/brands/service/brands.service.ts
+++ apps/backend/src/api/brands/service/brands.service.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {Repository} from "typeorm";
 import { Brand } from '@/api/brands/entities/brand.entity';
+import {CreateBrandDto} from "@/api/brands/dto/create-brand.dto";
+import {UpdateBrandDto} from "@/api/brands/dto/update-brand.dto";
 
 @Injectable()
 export class BrandsService {
-  constructor(@InjectRepository(Brand) brandsRepository) {}
+  @InjectRepository(Brand)
+  private readonly brandRepository: Repository<Brand>;
 
-  static findAll() {
-    return 'find all by service';
+  public create(body: CreateBrandDto) {
+    const newBrand = new Brand();
+
+    newBrand.name = body.name;
+    newBrand.description = body?.description;
+
+    return this.brandRepository.save(newBrand);
+  }
+
+  public async findAll() {
+    return await this.brandRepository.find();
+  }
+
+  public findOne(id: number) {
+    return this.brandRepository.findOne({ where: { id } });
+  }
+
+  public async update(id: number, body: UpdateBrandDto) {
+    const category = await this.findOne(id);
+
+    category.name = body?.name || category.name;
+    category.description = body.description || category.description;
+
+    return this.brandRepository.save(category);
+  }
+
+  public async delete(id: number) {
+    await this.brandRepository.delete({ id });
   }
 }
