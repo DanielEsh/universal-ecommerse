fc91e99c24f8a1ffe14f4c174bf565b1a6f01bfa
Daniel <daniilterawars@gmail.com>
2022-09-18 19:33:29 +0300

    feat: Breadcrumbs

--- apps/admin/components/ui/breadcrumbs/Breadcrumbs.tsx
+++ apps/admin/components/ui/breadcrumbs/Breadcrumbs.tsx
@@ -1,7 +1,31 @@
-// type BreadcrumbsProps = {
-//
-// }
+import ChevronRightIcon from 'public/icons/chevron-right.svg'
 
 export const Breadcrumbs = () => {
-    return <div>Breadcrumbs</div>
+    return (
+        <nav className="bg-grey-light rounded-md w-full">
+            <ol className="list-reset flex items-center gap-3">
+                <li>
+                    <a href="#" className="text-blue-500 hover:text-blue-700">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <div className="text-gray-800">
+                        <ChevronRightIcon />
+                    </div>
+                </li>
+                <li>
+                    <a href="#" className="text-blue-500 hover:text-blue-700">
+                        Library
+                    </a>
+                </li>
+                <li>
+                    <div className="text-gray-500">
+                        <ChevronRightIcon />
+                    </div>
+                </li>
+                <li className="text-gray-500">Data</li>
+            </ol>
+        </nav>
+    )
 }
