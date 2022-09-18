b0f2c0c2695b1fc6b429d7f0af8eb191480fa6c0
Daniel <daniilterawars@gmail.com>
2022-09-18 20:04:33 +0300

    feat(Breadcrumbs): generateBreadcrumbs

--- apps/admin/components/ui/breadcrumbs/Breadcrumbs.tsx
+++ apps/admin/components/ui/breadcrumbs/Breadcrumbs.tsx
@@ -1,30 +1,59 @@
+import { useRouter } from 'next/router'
 import ChevronRightIcon from 'public/icons/chevron-right.svg'
 
 export const Breadcrumbs = () => {
+    const router = useRouter()
+
+    const generateBreadcrumbs = () => {
+        // Remove any query parameters, as those aren't included in breadcrumbs
+        const asPathWithoutQuery = router.asPath.split('?')[0]
+
+        let idCounter = 1
+
+        // Break down the path between "/"s, removing empty entities
+        // Ex:"/my/nested/path" --> ["my", "nested", "path"]
+        const asPathNestedRoutes = asPathWithoutQuery
+            .split('/')
+            .filter((v) => v.length > 0)
+
+        // Iterate over the list of nested route parts and build
+        // a "crumb" object for each one.
+        const breadcrumbsList = asPathNestedRoutes.map((subpath, idx) => {
+            // We can get the partial nested route for the crumb
+            // by joining together the path parts up to this point.
+            const link = '/' + asPathNestedRoutes.slice(0, idx + 1).join('/')
+            // The title will just be the route string for now
+            const title = subpath
+            // make id
+            const id = idCounter++
+            return { link, title, id }
+        })
+
+        // Add in a default "Home" crumb for the top-level
+        return [{ id: 0, link: '/', title: 'Home' }, ...breadcrumbsList]
+    }
+
+    const renderBreadcrumbs = generateBreadcrumbs()
+
     return (
         <nav className="bg-grey-light rounded-md w-full">
             <ol className="list-reset flex items-center gap-3">
-                <li>
-                    <a href="#" className="text-blue-500 hover:text-blue-700">
-                        Home
-                    </a>
-                </li>
+                {renderBreadcrumbs.map(({ id, title, link }) => (
+                    <li key={id}>
+                        <a
+                            href={link}
+                            className="text-blue-500 hover:text-blue-700"
+                        >
+                            {title}
+                        </a>
+                    </li>
+                ))}
+
                 <li aria-hidden="true">
                     <div className="text-gray-800">
                         <ChevronRightIcon />
                     </div>
                 </li>
-                <li>
-                    <a href="#" className="text-blue-500 hover:text-blue-700">
-                        Library
-                    </a>
-                </li>
-                <li aria-hidden="true">
-                    <div className="text-gray-500">
-                        <ChevronRightIcon />
-                    </div>
-                </li>
-                <li className="text-black">Data</li>
             </ol>
         </nav>
     )
