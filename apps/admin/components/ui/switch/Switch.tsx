279c87489ebabd20e9f19fe2643a7d88ae79859f
Daniel <daniilterawars@gmail.com>
2022-09-23 22:12:01 +0300

    feat(Switch): toggle

--- apps/admin/components/ui/switch/Switch.tsx
+++ apps/admin/components/ui/switch/Switch.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import classnames from 'classnames'
+
 const COMPONENT_NAME = 'Switch'
 
 export type SwitchPropsType = {
@@ -9,9 +12,19 @@ export type SwitchPropsType = {
 }
 
 export const Switch = (props: SwitchPropsType) => {
+    const [toggle, setToggle] = useState<boolean>(false)
+
+    const handleClick = () => {
+        setToggle(!toggle)
+    }
+
+    const classes = classnames('inline-block h-6 w-6 transform rounded-full bg-white transition', {
+        ['translate-x-6']: toggle,
+    })
+
     return (
-        <button type="button" role="button" className="relative inline-flex h-6 w-12 items-center rounded-full bg-black">
-            <span className="inline-block h-6 w-6 transform rounded-full bg-white transition ui-checked:translate-x-6 ui-not-checked:translate-x-1"></span>
+        <button type="button" role="button" className="relative inline-flex h-6 w-12 items-center rounded-full bg-black" onClick={handleClick}>
+            <span className={classes}></span>
         </button>
     )
 }
