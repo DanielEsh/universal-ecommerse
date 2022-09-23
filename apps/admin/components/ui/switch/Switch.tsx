62beffbc97a38cb7fa61e66d71610a6c6c1e16a9
Daniel <daniilterawars@gmail.com>
2022-09-23 21:59:46 +0300

    feat(ui): Switch

--- apps/admin/components/ui/switch/Switch.tsx
+++ apps/admin/components/ui/switch/Switch.tsx
@@ -10,8 +10,8 @@ export type SwitchPropsType = {
 
 export const Switch = (props: SwitchPropsType) => {
     return (
-        <button type="button" role="button">
-            Switch
+        <button type="button" role="button" className="relative inline-flex h-6 w-12 items-center rounded-full bg-black">
+            <span className="inline-block h-6 w-6 transform rounded-full bg-white transition ui-checked:translate-x-6 ui-not-checked:translate-x-1"></span>
         </button>
     )
 }
