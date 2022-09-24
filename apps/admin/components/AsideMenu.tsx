c36c1abba8a1a8f3297a937cd6ef8f883e2312a3
Daniel <daniilterawars@gmail.com>
2022-09-24 23:42:52 +0300

    fix(AsideMenu): goods

--- apps/admin/components/AsideMenu.tsx
+++ apps/admin/components/AsideMenu.tsx
@@ -19,28 +19,23 @@ export const AsideMenu = () => {
         {
             id: 3,
             name: 'Товары',
-            slug: 'goods',
+            slug: '/goods',
         },
     ]
 
     const classes = (isActie: boolean) => {
         const activeStyles = `before:content-[''] before:absolute before:left-0 before:w-2 before:h-full before:bg-black before:rounded-r-md`
 
-        return classNames(
-            'relative flex items-center justify-center w-full h-[56px] rounded-md',
-            {
-                [activeStyles]: isActie,
-            },
-        )
+        return classNames('relative flex items-center justify-center w-full h-[56px] rounded-md', {
+            [activeStyles]: isActie,
+        })
     }
 
     return (
         <div className="flex flex-col gap-2">
             {list.map((item) => (
                 <Link key={item.id} href={item.slug}>
-                    <a className={classes(router.pathname == item.slug)}>
-                        {item.name}
-                    </a>
+                    <a className={classes(router.pathname == item.slug)}>{item.name}</a>
                 </Link>
             ))}
         </div>
