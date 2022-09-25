5dc46653bb701e3978b1ee27ec09cdeeeca0fc05
Daniel <daniilterawars@gmail.com>
2022-09-25 19:32:45 +0300

    feat(Switch): checked

--- apps/admin/components/layouts/header/Header.tsx
+++ apps/admin/components/layouts/header/Header.tsx
@@ -3,10 +3,14 @@ import { Switch } from '@/ui/switch/Switch'
 import { ThemeSwitcher } from '@/components/ThemeSwitcher'
 
 export const Header = () => {
+    const handleChange = (value: boolean) => {
+        console.log('CHANGE', value)
+    }
+
     return (
         <div className="sticky top-0 bg-gray-200 w-full h-[100px] dark:bg-neutral-900 dark:text-white">
             <HeaderBreadcrumbs />
-            <Switch />
+            <Switch checked onCheckedChange={handleChange} />
             <ThemeSwitcher />
         </div>
     )
--- apps/admin/components/ui/switch/Switch.tsx
+++ apps/admin/components/ui/switch/Switch.tsx
@@ -4,31 +4,41 @@ import classnames from 'classnames'
 const COMPONENT_NAME = 'Switch'
 
 export type SwitchPropsType = {
-    checked: boolean
-    disabled: boolean
+    checked?: boolean
+    disabled?: boolean
     onChange?: () => void
     onCheckedChange?: (checked: boolean) => void
 }
 
-export const Switch = forwardRef<HTMLButtonElement, SwitchPropsType>((props, forwardedRef) => {
-    const { onCheckedChange } = props
+export const Switch = forwardRef<HTMLButtonElement, SwitchPropsType>(
+    (props, forwardedRef) => {
+        const { checked, onCheckedChange } = props
 
-    const [toggle, setToggle] = useState<boolean>(false)
+        const [toggle, setToggle] = useState<boolean>(checked ?? false)
 
-    const handleClick = () => {
-        setToggle(!toggle)
-        onCheckedChange && onCheckedChange(toggle)
-    }
+        const handleClick = () => {
+            setToggle(!toggle)
+            onCheckedChange && onCheckedChange(toggle)
+        }
 
-    const classes = classnames('inline-block h-6 w-6 transform rounded-full bg-white transition', {
-        ['translate-x-full']: toggle,
-    })
+        const classes = classnames(
+            'inline-block h-6 w-6 transform rounded-full bg-white transition',
+            {
+                ['translate-x-full']: toggle,
+            },
+        )
 
-    return (
-        <button ref={forwardedRef} type="button" role="button" className="relative inline-flex h-6 w-12 items-center rounded-full bg-black" onClick={handleClick}>
-            <span className={classes}></span>
-        </button>
-    )
-})
+        return (
+            <button
+                ref={forwardedRef}
+                type="button"
+                role="button"
+                className="relative inline-flex h-6 w-12 items-center rounded-full bg-black"
+                onClick={handleClick}>
+                <span className={classes}></span>
+            </button>
+        )
+    },
+)
 
 Switch.displayName = COMPONENT_NAME
