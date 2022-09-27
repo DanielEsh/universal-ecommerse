72db4c10387b7925618ba5aae76405be0d0eb193
Daniel <daniilterawars@gmail.com>
2022-09-27 20:09:21 +0300

    feat(useToggle): toggle props

--- apps/admin/utils/ui/useToggle.tsx
+++ apps/admin/utils/ui/useToggle.tsx
@@ -5,5 +5,9 @@ export const useToggle = (initialState = false) => {
 
     const toggle = useCallback(() => setState((state) => !state), [])
 
-    return [state, toggle]
+    const toggleProps = {
+        'aria-checked': state,
+    }
+
+    return [state, toggle, toggleProps]
 }
