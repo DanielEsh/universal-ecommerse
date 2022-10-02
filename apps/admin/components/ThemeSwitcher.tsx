719fbf7a9899b4dd123d6f9d78d40a7403bdf33c
Daniel <daniilterawars@gmail.com>
2022-10-02 21:43:02 +0300

    feat(theme): change color-scheme

--- apps/admin/components/ThemeSwitcher.tsx
+++ apps/admin/components/ThemeSwitcher.tsx
@@ -6,6 +6,8 @@ export const ThemeSwitcher = () => {
     const MEDIA = '(prefers-color-scheme: dark)'
     const DOCUMENT_ELEMENT = document.documentElement
     const THEME_CLASS_TOGGLE_TOKEN = 'dark'
+    const COLORS_SCHEMES_LIST = ['light', 'dark']
+    const DEFAULT_THEME = 'system'
 
     const getSystemTheme = (event?: MediaQueryList | MediaQueryListEvent) => {
         if (!event) event = window.matchMedia(MEDIA)
@@ -13,10 +15,15 @@ export const ThemeSwitcher = () => {
         return isDark ? 'dark' : 'light'
     }
 
-    console.log(getSystemTheme())
+    const resolvedTheme = getSystemTheme()
 
     const handleSwitchTheme = () => {
         DOCUMENT_ELEMENT.classList.toggle(THEME_CLASS_TOGGLE_TOKEN)
+        const currentColorScheme = COLORS_SCHEMES_LIST.includes(resolvedTheme)
+            ? resolvedTheme
+            : DEFAULT_THEME
+
+        DOCUMENT_ELEMENT.style.colorScheme = currentColorScheme
     }
 
     return <div onClick={handleSwitchTheme}>Theme switcher</div>
