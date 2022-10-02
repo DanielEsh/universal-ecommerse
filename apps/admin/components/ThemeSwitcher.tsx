22ad6a8f6d0737007906d023afb26d233c5343dc
Daniel <daniilterawars@gmail.com>
2022-10-02 21:30:56 +0300

    feat(theme): getSystemTheme

--- apps/admin/components/ThemeSwitcher.tsx
+++ apps/admin/components/ThemeSwitcher.tsx
@@ -3,8 +3,17 @@ import { canUseDom } from '../utils/ui/canUseDom'
 export const ThemeSwitcher = () => {
     if (!canUseDom()) return ''
 
+    const MEDIA = '(prefers-color-scheme: dark)'
     const DOCUMENT_ELEMENT = document.documentElement
     const THEME_CLASS_TOGGLE_TOKEN = 'dark'
+
+    const getSystemTheme = (event?: MediaQueryList | MediaQueryListEvent) => {
+        if (!event) event = window.matchMedia(MEDIA)
+        const isDark = event.matches
+        return isDark ? 'dark' : 'light'
+    }
+
+    console.log(getSystemTheme())
 
     const handleSwitchTheme = () => {
         DOCUMENT_ELEMENT.classList.toggle(THEME_CLASS_TOGGLE_TOKEN)
