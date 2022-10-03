71f6c2eff414ff7c4806c292307fa2e6099abbea
Daniel <daniilterawars@gmail.com>
2022-10-03 23:34:56 +0300

    feat(theme): getTheme

--- apps/admin/components/ThemeSwitcher.tsx
+++ apps/admin/components/ThemeSwitcher.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react'
 import { canUseDom } from '../utils/ui/canUseDom'
 
-export const ThemeSwitcher = () => {
-    if (!canUseDom()) return ''
+enum Theme {
+    light = 'light',
+    dark = 'dark',
+    system = 'system',
+}
 
-    const MEDIA = '(prefers-color-scheme: dark)'
-    const DOCUMENT_ELEMENT = document.documentElement
+export const ThemeSwitcher = () => {
     const THEME_CLASS_TOGGLE_TOKEN = 'dark'
     const COLORS_SCHEMES_LIST = ['light', 'dark']
-    const DEFAULT_THEME = 'system'
+    const DEFAULT_THEME = Theme.system
+    const MEDIA = '(prefers-color-scheme: dark)'
+    const STORAGE_KEY = 'theme'
+
+    const [theme, setThemeState] = useState(() =>
+        getTheme(STORAGE_KEY, DEFAULT_THEME),
+    )
+    const [resolvedTheme, setResolvedTheme] = useState(() =>
+        getTheme(STORAGE_KEY),
+    )
+
+    if (!canUseDom()) return ''
+
+    const DOCUMENT_ELEMENT = document.documentElement
 
     const getSystemTheme = (event?: MediaQueryList | MediaQueryListEvent) => {
         if (!event) event = window.matchMedia(MEDIA)
@@ -15,7 +31,17 @@ export const ThemeSwitcher = () => {
         return isDark ? 'dark' : 'light'
     }
 
-    const resolvedTheme = getSystemTheme()
+    function getTheme(key: string, fallback?: string) {
+        let theme
+        try {
+            theme = localStorage.getItem(key) || undefined
+        } catch (e) {
+            // Unsupported
+        }
+        return theme || fallback
+    }
+
+    // const resolvedTheme = getSystemTheme()
 
     const handleSwitchTheme = () => {
         DOCUMENT_ELEMENT.classList.toggle(THEME_CLASS_TOGGLE_TOKEN)
