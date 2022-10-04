cbc1a6061b8c1688b0933f7f6ba228f34128b4df
Daniel <daniilterawars@gmail.com>
2022-10-04 22:00:52 +0300

    feat(theme): useTheme

--- apps/admin/utils/theme/useTheme.tsx
+++ apps/admin/utils/theme/useTheme.tsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { getTheme } from './getTheme'
+import { getSystemTheme } from './getSystemTheme'
 
 enum Theme {
     light = 'light',
@@ -9,22 +10,35 @@ enum Theme {
 
 const DEFAULT_THEME = Theme.system
 const STORAGE_KEY = 'theme'
+const colorSchemes = ['light', 'dark']
 
 export const useTheme = () => {
     const [theme, setThemeState] = useState(() =>
         getTheme(STORAGE_KEY, DEFAULT_THEME),
     )
 
-    const applyTheme = (theme) => {
+    const applyTheme = useCallback((theme) => {
+        let resolved = theme
         setThemeState(theme)
+        if (!resolved) return
 
-        // Save to storage
-        try {
-            localStorage.setItem(STORAGE_KEY, theme)
-        } catch (e) {
-            // Unsupported
+        // If theme is system, resolve it before setting theme
+        if (theme === 'system') {
+            resolved = getSystemTheme()
         }
-    }
+
+        const d = document.documentElement
+
+        d.classList.add('dark')
+
+        const fallback = colorSchemes.includes(DEFAULT_THEME)
+            ? DEFAULT_THEME
+            : null
+        const colorScheme = colorSchemes.includes(resolved) ? theme : fallback
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        d.style.colorScheme = colorScheme
+    }, [])
 
     const changeTheme = (changedTheme) => {
         const themesActions = {
