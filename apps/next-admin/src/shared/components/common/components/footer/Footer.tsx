5719b89a01daf82ebd0b6d619858956326344624
Daniel <daniilterawars@gmail.com>
2022-12-04 20:45:40 +0300

    feat: footer

---
+++ apps/next-admin/src/shared/components/common/components/footer/Footer.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export const Footer = () => {
+  return (
+    <footer className="flex h-[64px] items-center border-t border-gray-300 px-6">
+      <div>2022 Universal admin</div>
+    </footer>
+  )
+}
--- apps/next-admin/src/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
+++ apps/next-admin/src/shared/components/layouts/DefaultLayout/DefaultLayout.tsx
@@ -3,6 +3,7 @@ import { clsx } from 'clsx'
 import { Sidebar } from '@/src/shared/components/common/components/sidebar/Sidebar'
 import { useSidebar } from '@/src/shared/model'
 import { Header } from '@/src/shared/components/common/components/header/Header'
+import { Footer } from '@/src/shared/components/common/components/footer/Footer'
 
 export const DefaultLayout = ({ children }: PropsWithChildren) => {
   const sidebarToggled = useSidebar()
@@ -18,7 +19,8 @@ export const DefaultLayout = ({ children }: PropsWithChildren) => {
       <div className={contentClasses}>
         <Header />
 
-        <div className="mt-[64px] bg-stone-900 p-6 text-white">{children}</div>
+        <div className="mt-[64px] p-6 text-black">{children}</div>
+        <Footer />
       </div>
     </div>
   )
