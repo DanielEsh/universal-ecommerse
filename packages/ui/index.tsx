fced9c922b751fc3cb88150cdf166a12a867152e
Daniel <daniilterawars@gmail.com>
2022-12-04 19:41:52 +0300

    feat(ui): dropdown

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,3 +3,4 @@ export * from "./Button";
 export * from "./src/components/portal"
 export * from "./src/components/modal"
 export * from "./src/components/breadcrumbs"
+export * from "./src/components/dropdown"
