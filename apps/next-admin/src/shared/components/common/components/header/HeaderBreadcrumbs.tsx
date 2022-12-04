6025ff787a62d3bb69960a88638e922ae5c3e2bf
Daniel <daniilterawars@gmail.com>
2022-12-04 18:02:22 +0300

    feat(header): breadcrumbs

--- apps/next-admin/src/shared/components/common/components/header/HeaderBreadcrumbs.tsx
+++ apps/next-admin/src/shared/components/common/components/header/HeaderBreadcrumbs.tsx
@@ -1,49 +1,52 @@
 import { useMemo } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { Breadcrumbs } from 'ui'
 
 export const HeaderBreadcrumbs = () => {
-  //   const router = useRouter()
+  const router = useRouter()
+  const pathname = usePathname()
 
-  //   const generateBreadcrumbs = () => {
-  //     // Remove any query parameters, as those aren't included in breadcrumbs
-  //     const asPathWithoutQuery = router.asPath.split('?')[0]
+  const generateBreadcrumbs = () => {
+    if (!pathname) {
+      return
+    }
 
-  //     // Break down the path between "/"s, removing empty entities
-  //     // Ex:"/my/nested/path" --> ["my", "nested", "path"]
-  //     const asPathNestedRoutes = asPathWithoutQuery
-  //       .split('/')
-  //       .filter((v) => v.length > 0)
+    // Remove any query parameters, as those aren't included in breadcrumbs
+    const asPathWithoutQuery = pathname.split('?')[0]
 
-  //     // Iterate over the list of nested route parts and build
-  //     // a "crumb" object for each one.
-  //     const breadcrumbsList = asPathNestedRoutes.map((subpath, idx) => {
-  //       // We can get the partial nested route for the crumb
-  //       // by joining together the path parts up to this point.
-  //       const link = '/' + asPathNestedRoutes.slice(0, idx + 1).join('/')
-  //       // The title will just be the route string for now
-  //       const title = subpath
-  //       return { link, title }
-  //     })
+    // Break down the path between "/"s, removing empty entities
+    // Ex:"/my/nested/path" --> ["my", "nested", "path"]
+    const asPathNestedRoutes = asPathWithoutQuery
+      .split('/')
+      .filter((v) => v.length > 0)
 
-  //     // Add in a default "Home" crumb for the top-level
-  //     return [{ link: '/', title: 'Home' }, ...breadcrumbsList]
-  //   }
+    // Iterate over the list of nested route parts and build
+    // a "crumb" object for each one.
+    const breadcrumbsList = asPathNestedRoutes.map((subpath, idx) => {
+      // We can get the partial nested route for the crumb
+      // by joining together the path parts up to this point.
+      const link = '/' + asPathNestedRoutes.slice(0, idx + 1).join('/')
+      // The title will just be the route string for now
+      const title = subpath
+      return { link, title }
+    })
 
-  const renderBreadcrumbs = [
-    { link: '/', title: 'Home' },
-    { link: '/', title: 'Test' },
-  ]
+    // Add in a default "Home" crumb for the top-level
+    return [{ link: '/', title: 'Home' }, ...breadcrumbsList]
+  }
 
-  //   const renderBreadcrumbs = useMemo(
-  //     () => generateBreadcrumbs(),
-  //     [router.asPath],
-  //   )
+  //   const renderBreadcrumbs = [
+  //     { link: '/', title: 'Home' },
+  //     { link: '/', title: 'Test' },
+  //   ]
+
+  const renderBreadcrumbs = useMemo(() => generateBreadcrumbs(), [pathname])
 
   return (
     <Breadcrumbs>
-      {renderBreadcrumbs.map(({ title, link }, index) => (
+      {renderBreadcrumbs?.map(({ title, link }, index) => (
         <Breadcrumbs.Item
           key={index}
           isLast={index === renderBreadcrumbs.length - 1}>
