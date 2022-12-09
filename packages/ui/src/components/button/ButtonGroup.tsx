dc922dc90e6cbfc701de6b68a3f1cf944e4ec7f6
Daniel <daniilterawars@gmail.com>
2022-12-09 22:22:05 +0300

    feat(ui): ButtonGroup

--- packages/ui/src/components/button/ButtonGroup.tsx
+++ packages/ui/src/components/button/ButtonGroup.tsx
@@ -1,3 +1,20 @@
-export const ButtonGroup = () => {
-  return <div>Button Group</div>
+import { PropsWithChildren } from 'react'
+import { BaseButton } from './Button'
+
+export const GroupedButton = BaseButton
+
+export const ButtonGroup = ({ children }: PropsWithChildren) => {
+  return (
+    <div role="group" className="inline-flex rounded-md shadow-sm">
+      <BaseButton className="ripple-root bg-black text-white rounded-l py-4 px-8">
+        Button 1
+      </BaseButton>
+      <BaseButton className="ripple-root bg-black text-white rounded-none py-4 px-8">
+        Button 2
+      </BaseButton>
+      <BaseButton className="ripple-root bg-black text-white rounded-r py-4 px-8">
+        Button 3
+      </BaseButton>
+    </div>
+  )
 }
