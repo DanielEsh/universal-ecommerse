e7227c8fb370fc4efa09a02250da864a24ce5751
Daniel <daniilterawars@gmail.com>
2022-12-12 22:02:32 +0300

    feat(ui): group button

--- packages/ui/src/components/button/ButtonGroup.tsx
+++ packages/ui/src/components/button/ButtonGroup.tsx
@@ -1,25 +1,16 @@
-import { PropsWithChildren } from 'react'
+import { ReactNode } from 'react'
 import { BaseButton } from './Button'
 
 export const GroupedButton = BaseButton
 
-export const ButtonGroup = ({ children }: PropsWithChildren) => {
+export type ButtonGroupProps = {
+  children: ReactNode
+}
+
+export const ButtonGroup = ({ children }: ButtonGroupProps) => {
   return (
-    <div
-      role="group"
-      className="inline-flex rounded-md shadow-sm border border-secondary-500">
-      <BaseButton className="ripple-root rounded-l py-2 px-4 border-r border-secondary-500">
-        Button 1
-      </BaseButton>
-      <BaseButton className="ripple-root rounded-none py-2 px-4 border-r border-secondary-500">
-        Button 2
-      </BaseButton>
-      <BaseButton className="ripple-root rounded-none py-2 px-4 border-r border-secondary-500">
-        Button 2
-      </BaseButton>
-      <BaseButton className="ripple-root rounded-r py-2 px-4">
-        Button 3
-      </BaseButton>
+    <div role="group" className="inline-flex rounded-md shadow-sm">
+      {children}
     </div>
   )
 }
--- packages/ui/src/components/button/__examples__/ButtonGroup.tsx
+++ packages/ui/src/components/button/__examples__/ButtonGroup.tsx
@@ -1,9 +1,30 @@
-import { ButtonGroup } from '../ButtonGroup'
+import { ButtonGroup, GroupedButton } from '../ButtonGroup'
 
 export default function () {
   return (
     <div>
-      <ButtonGroup />
+      <ButtonGroup>
+        <GroupedButton
+          className="rounded-r-none border-r border-black"
+          variant="outlined">
+          Button 1
+        </GroupedButton>
+        <GroupedButton
+          className="rounded-none border-r border-black"
+          variant="outlined">
+          Button 2
+        </GroupedButton>
+        <GroupedButton
+          className="rounded-none border-r border-black"
+          variant="outlined">
+          Button 2
+        </GroupedButton>
+        <GroupedButton
+          className="rounded-l-none border-r border-black"
+          variant="outlined">
+          Button 3
+        </GroupedButton>
+      </ButtonGroup>
     </div>
   )
 }
