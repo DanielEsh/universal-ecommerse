36baa46f2ae6fff8f4f282c5b04a1292fe206899
Daniel <daniilterawars@gmail.com>
2023-01-07 17:00:03 +0300

    feat(ui): exports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,15 @@
+// Components
 export * from '@/components/portal'
 export * from '@/components/modal'
 export * from '@/components/breadcrumbs'
 export * from '@/components/dropdown'
 export * from '@/components/menu'
+
+// Utils
+export * from '@/utils/canUseDOM'
+
+// Hooks
+export * from '@/hooks/useClickOutside'
+export * from '@/hooks/useComposedRefs'
+export * from '@/hooks/useEventListener'
+export * from '@/hooks/useKeyPress'
