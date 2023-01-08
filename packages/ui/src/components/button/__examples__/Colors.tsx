39c30b4f4f1fe80e3418acbda9589796845a3a6f
Daniel <daniilterawars@gmail.com>
2023-01-08 15:57:41 +0300

    feat(ui): button colors

--- packages/ui/src/components/button/__examples__/Colors.tsx
+++ packages/ui/src/components/button/__examples__/Colors.tsx
@@ -48,6 +48,81 @@ const Colors = () => {
           <LeftIcon />
         </BaseButton>
       </div>
+
+      <h2>Success</h2>
+      <div className="flex gap-3">
+        <BaseButton color="success">
+          <BaseButton.Addon>
+            <LeftIcon />
+          </BaseButton.Addon>
+          Button
+          <BaseButton.Addon>
+            <RightIcon />
+          </BaseButton.Addon>
+        </BaseButton>
+        <BaseButton color="success" variant="outlined">
+          Outlined
+        </BaseButton>
+        <BaseButton color="success" variant="ghost">
+          Ghost
+        </BaseButton>
+        <BaseButton color="success" disabled>
+          Disabled
+        </BaseButton>
+        <BaseButton color="success">
+          <LeftIcon />
+        </BaseButton>
+      </div>
+
+      <h2>Warning</h2>
+      <div className="flex gap-3">
+        <BaseButton color="warning">
+          <BaseButton.Addon>
+            <LeftIcon />
+          </BaseButton.Addon>
+          Button
+          <BaseButton.Addon>
+            <RightIcon />
+          </BaseButton.Addon>
+        </BaseButton>
+        <BaseButton color="warning" variant="outlined">
+          Outlined
+        </BaseButton>
+        <BaseButton color="warning" variant="ghost">
+          Ghost
+        </BaseButton>
+        <BaseButton color="warning" disabled>
+          Disabled
+        </BaseButton>
+        <BaseButton color="warning">
+          <LeftIcon />
+        </BaseButton>
+      </div>
+
+      <h2>Error</h2>
+      <div className="flex gap-3">
+        <BaseButton color="error">
+          <BaseButton.Addon>
+            <LeftIcon />
+          </BaseButton.Addon>
+          Button
+          <BaseButton.Addon>
+            <RightIcon />
+          </BaseButton.Addon>
+        </BaseButton>
+        <BaseButton color="error" variant="outlined">
+          Outlined
+        </BaseButton>
+        <BaseButton color="error" variant="ghost">
+          Ghost
+        </BaseButton>
+        <BaseButton color="error" disabled>
+          Disabled
+        </BaseButton>
+        <BaseButton color="error">
+          <LeftIcon />
+        </BaseButton>
+      </div>
     </div>
   )
 }
