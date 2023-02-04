e093c9a228afc00745a6f241241c0018dfcf9a5a
Daniel <daniilterawars@gmail.com>
2023-02-05 01:44:31 +0300

    feat(admin): mocks

--- apps/next-admin/src/mocks/handlers/index.ts
+++ apps/next-admin/src/mocks/handlers/index.ts
@@ -3,10 +3,10 @@ import { test } from './test'
 import brandsData from './brandsData.json'
 
 export const handlers = [
+  // rest.get('https://my.backend/book', (_req, res, ctx) => {
+  //   return res(ctx.json(test))
+  // }),
   rest.get('https://my.backend/book', (_req, res, ctx) => {
-    return res(ctx.json(test))
-  }),
-  rest.get('api/brands', (_req, res, ctx) => {
     return res(ctx.json(brandsData))
   }),
 ]
--- apps/next-admin/src/shared/api/brands/index.ts
+++ apps/next-admin/src/shared/api/brands/index.ts
@@ -2,6 +2,6 @@ import { $axios } from '@/src/shared/lib/axios'
 import { Brand } from '@/src/entities/brands'
 
 export const getAllBrands = async () => {
-  const { data } = await $axios.get<Brand[]>('/brands')
+  const { data } = await $axios.get<Brand[]>('https://my.backend/book')
   return data
 }
