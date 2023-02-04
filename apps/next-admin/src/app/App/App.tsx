da60d6aac946f3620372e05cdb0474cfcd026182
Daniel <daniilterawars@gmail.com>
2023-02-04 21:14:31 +0300

    fix(next-admin): mocks

--- apps/next-admin/src/app/App/App.tsx
+++ apps/next-admin/src/app/App/App.tsx
@@ -8,17 +8,11 @@ type Props = {
   children: ReactNode
 }
 
-// if (process.env.NEXT_PUBLIC_API_MOCKING) {
-//   console.log(process.env.NEXT_PUBLIC_API_MOCKING)
-//   initMsw()
-// }
+// Init MSW
+if (process.env.NEXT_PUBLIC_API_MOCKING) {
+  initMsw()
+}
 
 export const App = ({ children }: Props) => {
-  console.log('INIT APP')
-  // Init MSW
-  // ;(async () => {
-  //   await initMsw()
-  // })()
-
   return <ReactQueryWrapper>{children}</ReactQueryWrapper>
 }
