c9af9cb321061a7d57635dc3df82a413a62e98bc
Daniel <daniilterawars@gmail.com>
2023-02-08 06:53:33 +0300

    feat(admin): table resize column

--- apps/next-admin/src/entities/brands/ui/BrandsTable.tsx
+++ apps/next-admin/src/entities/brands/ui/BrandsTable.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+
 import { Brand } from '@/src/entities/brands'
 import { Table } from '@/src/shared/ui/Table'
 import { BrandsTableActions } from '@/src/entities/brands/ui/BrandsTableActions'
@@ -8,17 +10,63 @@ interface Props {
 }
 
 export const BrandsTable = ({ brands }: Props) => {
+  const move = () => {
+    console.log('MOVE')
+  }
+
+  const up = () => {
+    console.log('UP')
+    handleResizeEnd()
+  }
+
+  const bindMove = () => {
+    document.addEventListener('mousemove', move)
+  }
+
+  const bindUp = () => {
+    document.addEventListener('mouseup', up)
+  }
+
+  const unbindMove = () => {
+    document.removeEventListener('mousemove', move)
+  }
+
+  const unbindUp = () => {
+    document.removeEventListener('mouseup', up)
+  }
+
+  const handleResizerClick = (event: any) => {
+    console.log('click', event)
+  }
+
+  const handleResizeStart = (event: any) => {
+    console.log('start', event)
+    bindMove()
+    bindUp()
+  }
+
+  function handleResizeEnd() {
+    console.log('resize end')
+    unbindMove()
+    unbindUp()
+  }
+
   return (
     <Table>
       <Table.Caption>
         <BrandsTableHeader />
       </Table.Caption>
       <Table.Head>
-        <Table.CellHead>Number</Table.CellHead>
-        <Table.CellHead>Slug</Table.CellHead>
-        <Table.CellHead>Name</Table.CellHead>
-        <Table.CellHead>Count</Table.CellHead>
-        <Table.CellHead>Actions</Table.CellHead>
+        <Table.CellHead
+          resizable
+          onResizerClick={handleResizerClick}
+          onResizeStart={handleResizeStart}>
+          Number
+        </Table.CellHead>
+        <Table.CellHead resizable>Slug</Table.CellHead>
+        <Table.CellHead resizable>Name</Table.CellHead>
+        <Table.CellHead resizable>Count</Table.CellHead>
+        <Table.CellHead resizable>Actions</Table.CellHead>
       </Table.Head>
 
       <Table.Body>
--- apps/next-admin/src/shared/ui/Table/TableCellHead.tsx
+++ apps/next-admin/src/shared/ui/Table/TableCellHead.tsx
@@ -4,10 +4,13 @@ import { TableContext } from '@/src/shared/ui/Table/TableContext'
 
 export type TableCellProps = {
   children: ReactNode
+  resizable?: boolean
+  onResizerClick?: (e: any) => void
+  onResizeStart?: (e: any) => void
 }
 
 export const TableCellHead = (props: TableCellProps) => {
-  const { children } = props
+  const { children, resizable, onResizerClick, onResizeStart } = props
 
   const { color } = useContext(TableContext)
 
@@ -15,7 +18,35 @@ export const TableCellHead = (props: TableCellProps) => {
     primary: 'p-2 border',
   }
 
-  const classes = classNames(colorsList[color])
+  const classes = classNames(colorsList[color], {
+    relative: resizable,
+  })
 
-  return <th className={classes}>{children}</th>
+  const handleResizerClick = (event: any) => {
+    if (onResizerClick) {
+      onResizerClick({
+        originalEvent: event,
+      })
+    }
+  }
+
+  const handleMouseDown = (event: any) => {
+    if (onResizeStart)
+      onResizeStart({
+        originalEvent: event,
+      })
+  }
+
+  return (
+    <th className={classes}>
+      {resizable && (
+        <span
+          className="absolute top-0 right-0 h-full w-2 cursor-col-resize bg-red-500"
+          onMouseDown={handleMouseDown}
+          onClick={handleResizerClick}
+        />
+      )}
+      {children}
+    </th>
+  )
 }
