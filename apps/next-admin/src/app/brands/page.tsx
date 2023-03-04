e737364c28014c32e9548a37a5116bceab78df57
Daniel <daniilterawars@gmail.com>
2023-03-04 23:09:02 +0300

    fix: page

--- apps/next-admin/src/app/brands/page.tsx
+++ apps/next-admin/src/app/brands/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import { useGetAllBrands } from '@/src/shared/api/brands/queries'
+
 export default function BrandsPage() {
-  return <></>
+  const { isLoading, data: brands } = useGetAllBrands()
+
+  return (
+    <div>
+      {isLoading && <div>Loading...</div>}
+
+      {!isLoading && brands && <div>{JSON.stringify(brands)}</div>}
+    </div>
+  )
 }
